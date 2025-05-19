0ddb930328f94ea87a4b303adae7d5fa958ec576
eedrisofficial <eedrisharuna3@gmail.com>
2025-05-19 15:15:26 +0100

    added social media as alternative

--- components/JobStructuredData.tsx
+++ components/JobStructuredData.tsx
@@ -5,6 +5,7 @@ export default function JobStructuredData({ job }: { job: JobType }) {
   const structuredData = {
     "@context": "https://schema.org/",
     "@type": "JobPosting",
+    url: "https://www.shamzbridgeconsult.org/careers",
     title: job.title,
     location: job.location,
     datePosted: job.createdAt,
@@ -13,8 +14,12 @@ export default function JobStructuredData({ job }: { job: JobType }) {
     mode: job.mode,
     hiringOrganization: {
       "@type": "Organization",
+      url: "https://www.shamzbridgeconsult.org/",
       name: "Shamzbridge Consult",
-      sameAs: "https://www.shamzbridgeconsult.org/",
+      sameAs: [
+        "https://www.linkedin.com/company/shamzbridge-consult/",
+        "https://x.com/shamzbridge",
+      ],
       logo: "https://cdn.hashnode.com/res/hashnode/image/upload/v1724055539467/36a4b661-bd4f-488c-88d9-7109189330e5.png",
     },
     jobLocation: {
